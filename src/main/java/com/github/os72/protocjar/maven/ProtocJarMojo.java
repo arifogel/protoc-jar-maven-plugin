06c98663dd9ad7c444b37c987e7d66d1c56bbdd8
Ari Fogel <ari@fogelti.me>
2018-06-28 13:06:50 -0700

    clear messages before adding new ones

--- src/main/java/com/github/os72/protocjar/maven/ProtocJarMojo.java
+++ src/main/java/com/github/os72/protocjar/maven/ProtocJarMojo.java
@@ -491,28 +491,29 @@ public class ProtocJarMojo extends AbstractMojo
 	private void processFile(File file, String version, String type, String pluginPath, File outputDir, String outputOptions) throws MojoExecutionException {
 		getLog().info("    Processing ("+ type + "): " + file.getName());
 		Collection<String> cmd = buildCommand(file, version, type, pluginPath, outputDir, outputOptions);
+		buildContext.removeMessages(file);
 		try {
 			int ret = 0;
-      ByteArrayOutputStream out = new ByteArrayOutputStream();
-      ByteArrayOutputStream err = new ByteArrayOutputStream();
-      TeeOutputStream outTee = new TeeOutputStream(System.out, out);
-      TeeOutputStream errTee = new TeeOutputStream(System.err, err);
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ByteArrayOutputStream err = new ByteArrayOutputStream();
+			TeeOutputStream outTee = new TeeOutputStream(System.out, out);
+			TeeOutputStream errTee = new TeeOutputStream(System.err, err);
 			if (protocCommand == null) ret = Protoc.runProtoc(cmd.toArray(new String[0]), outTee, errTee);
 			else ret = Protoc.runProtoc(protocCommand, Arrays.asList(cmd.toArray(new String[0])), outTee, errTee);
-      String outStr = out.toString();
-      String errStr = err.toString();
-      if (!errStr.isEmpty()) {
-        int severity = ret != 0 ? BuildContext.SEVERITY_ERROR : BuildContext.SEVERITY_WARNING;
-        String[] lines = errStr.split("\\n",-1);
-        for (String line : lines) {
-          if (line.contains(file.getName().toString())) {
-            String[] parts = line.split(":",4);
-            if (parts.length == 4) {
-              buildContext.addMessage(file, Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parts[3], severity, null);
-            }
-          }
-        }
-      }
+			String outStr = out.toString();
+			String errStr = err.toString();
+			if (!errStr.isEmpty()) {
+				int severity = ret != 0 ? BuildContext.SEVERITY_ERROR : BuildContext.SEVERITY_WARNING;
+				String[] lines = errStr.split("\\n",-1);
+				for (String line : lines) {
+					if (line.contains(file.getName().toString())) {
+						String[] parts = line.split(":",4);
+						if (parts.length == 4) {
+							buildContext.addMessage(file, Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), parts[3], severity, null);
+						}
+					}
+				}
+			}
 			if (ret != 0) throw new MojoExecutionException("protoc-jar failed for " + file + ". Exit code " + ret);
 		}
 		catch (InterruptedException e) {
